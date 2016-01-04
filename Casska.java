6695b2d03961ed96b79d7e5d4f6a0d0f57733c0e
Slawomir Weclewski MAC <wentilo@gmail.com>
2016-01-04 15:07:55 +0100

    Dodalem Casska

---
+++ Casska.java
@@ -0,0 +1,47 @@
+/**
+ * Próba podłączenia się do Cassandry i wykonanie prostych instrukcji na bazie.
+ * Wszystkie zewnętrzne biblioteki dodałem ręcznie, plik po pliku. 
+ */
+
+package slawissimo;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+/**
+ * @author wentilo
+ *
+ */
+public class Casska {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		createKeyspace();
+
+			}
+	
+		public static void createKeyspace()
+		{
+			Cluster cluster = Cluster.builder()
+	                 .addContactPoints("127.0.0.1")
+	                 .build();
+			Session session = cluster.connect();
+			String cqlStatement = "CREATE KEYSPACE testy1 WITH " + 
+			                     "replication = {'class':'SimpleStrategy','replication_factor':1}";        
+			session.execute(cqlStatement);
+			
+			String cqlStatement2 = "CREATE TABLE testy1.users (" + 
+			                      " user_name varchar PRIMARY KEY," + 
+			                      " password varchar " + 
+			                      ");";
+			session.execute(cqlStatement2);
+			
+			System.out.println("Done");
+			System.exit(0);
+			
+			}
+}
+
